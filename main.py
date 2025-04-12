870b4953f21b188ce0ee12e82ca263c020cf0b10
coder7475 <robiulhossain7475@gmail.com>
2025-04-12 08:28:40 +0600

    feat: create a todo list

--- main.py
+++ main.py
@@ -3,6 +3,31 @@ from fastapi import FastAPI
 api = FastAPI()
 
 
+all_todos = [
+    {
+        "todo_id": 1,
+        "todo_name": "Exercise",
+        "todo_description": "Exercise every morning for 30 minutes",
+    },
+    {"todo_id": 2, "todo_name": "Read", "todo_description": "Read 20 pages of a book"},
+    {
+        "todo_id": 3,
+        "todo_name": "Work on Project",
+        "todo_description": "Spend 2 hours on personal project",
+    },
+    {
+        "todo_id": 4,
+        "todo_name": "Learn",
+        "todo_description": "Watch one tutorial on Docker",
+    },
+    {
+        "todo_id": 5,
+        "todo_name": "Plan Next Day",
+        "todo_description": "Write down tasks for tomorrow",
+    },
+]
+
+
 @api.get("/")
 def index():
     return {"message": "Hello World"}
