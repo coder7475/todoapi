b7060caf0e17cea0d2d425bc4fca66f10ea6a3f7
coder7475 <robiulhossain7475@gmail.com>
2025-04-12 10:45:54 +0600

    feat: create a gunicorn config

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,2 +1,13 @@
-bind = "0.0.0.0:8000"
-workers = 4
+from multiprocessing import cpu_count
+
+# Socket Path
+bind = "unix:/home/todoapi/gunicorn.sock"
+
+# Worker Options
+workers = cpu_count()
+worker_class = "uvicorn.workers.UvicornWorker"
+
+# Logging Options
+loglevel = "debug"
+accesslog = "/home/todoapi/access_log"
+errorlog = "/home/todoapi/error_log"
